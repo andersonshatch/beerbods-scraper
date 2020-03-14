c59c0bbaac639dcc0df678f6ad62b2caca6be466
Josh Anderson <andersonshatch@gmail.com>
2020-03-14 21:45:20 +0000

    Get branches instead of pulls

--- functions/test.js
+++ functions/test.js
@@ -2,7 +2,7 @@ request = require('request');
 exports.handler = function(event, context, callback) {
 	let identity = context.clientContext.identity;
 	let bearer = identity.token;
-	let url = identity.url.replace('/identity', '/git/github/pulls');
+	let url = identity.url.replace('/identity', '/git/github/branches');
 
 	request(url, {'auth': {'bearer': bearer}}, function(error, response, body) {
 		callback(null, {
