d95b2a448b3050aea2eac29f0652075ac6e10417
Josh Anderson <andersonshatch@gmail.com>
2020-03-14 21:13:42 +0000

    Print deploy URL in response

--- functions/test.js
+++ functions/test.js
@@ -1,7 +1,8 @@
 request = require('request');
 exports.handler = function(event, context, callback) {
+
 	callback(null, {
 		statusCode: 200,
-		body: "Hi!",
+		body: `I'm at ${process.env.DEPLOY_URL}`,
 	});
 }
