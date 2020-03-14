71f79d26d590e7b90dd0f1c25fc5e2894b116ada
Josh Anderson <andersonshatch@gmail.com>
2020-03-14 21:40:38 +0000

    Try a request to git gateway

--- functions/test.js
+++ functions/test.js
@@ -1,8 +1,15 @@
 request = require('request');
 exports.handler = function(event, context, callback) {
+	let identity = context.clientContext.identity;
+	let bearer = identity.token;
+	let url = identity.url.replace('/identity', '/git/github/pulls');
 
-	callback(null, {
-		statusCode: 200,
-		body: JSON.stringify({'event': event, 'context': context}),
+	request(url, {'auth': {'bearer': bearer}}, function(error, response, body) {
+		callback(null, {
+			statusCode: 200,
+			body: JSON.stringify({'error': error, 'response': response, 'body': body}),
+		});
 	});
+
+
 }
